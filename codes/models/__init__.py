659644dd61f8f7b2e2fa4354b46204f8ad1829e3
victorca25 <41912303+victorca25@users.noreply.github.com>
2019-06-30 10:20:43 +0200

    Added modified model options

--- codes/models/__init__.py
+++ codes/models/__init__.py
@@ -13,6 +13,12 @@ def create_model(opt):
         from .SRRaGAN_model import SRRaGANModel as M
     elif model == 'sftgan':
         from .SFTGAN_ACD_model import SFTGAN_ACD_Model as M
+    elif model == 'srragan_hfen':
+        from .SRRaGAN_hfen_model import SRRaGANModel as M
+    elif model == 'srragan_n2n':
+        from .SRRaGAN_n2n_model import SRRaGANModel as M
+    elif model == 'ESPCN':
+        from .ESPCN_model import ESPCNModel as M
     else:
         raise NotImplementedError('Model [{:s}] not recognized.'.format(model))
     m = M(opt)
