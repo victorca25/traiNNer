d499d875453009ac201649fec677a7cf4033559c
victorca25 <41912303+victorca25@users.noreply.github.com>
2019-07-10 21:28:04 +0200

    Add the Charbonnier Loss
    As implemented in the BasicSR code

--- codes/models/modules/loss.py
+++ codes/models/modules/loss.py
@@ -66,6 +66,18 @@ class GaussianSmoothing(nn.Module):
     def forward(self, input):
         return self.conv(input, weight=self.weight, groups=self.groups)
 
+class CharbonnierLoss(nn.Module):
+    """Charbonnier Loss (L1)"""
+
+    def __init__(self, eps=1e-6):
+        super(CharbonnierLoss, self).__init__()
+        self.eps = eps
+
+    def forward(self, x, y):
+        diff = x - y
+        loss = torch.sum(torch.sqrt(diff * diff + self.eps))
+        return loss
+    
 # Define GAN loss: [vanilla | lsgan | wgan-gp]
 class GANLoss(nn.Module):
     def __init__(self, gan_type, real_label_val=1.0, fake_label_val=0.0):
