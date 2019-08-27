74ba9a44f0400fd362ac069c06cc53642d337987
victorca25 <41912303+victorca25@users.noreply.github.com>
2019-08-27 07:21:14 +0200

    Update train_ppon.py

--- codes/train_ppon.py
+++ codes/train_ppon.py
@@ -7,7 +7,6 @@ import random
 import numpy as np
 from collections import OrderedDict
 import logging
-import glob
 
 import torch
 
@@ -23,25 +22,30 @@ def main():
     parser.add_argument('-opt', type=str, required=True, help='Path to option JSON file.')
     opt = option.parse(parser.parse_args().opt, is_train=True)
     opt = option.dict_to_nonedict(opt)  # Convert to NoneDict, which return None for missing key.
-
-    # training from scratch
-    if not opt['path']['resume_state']:
-        resume_state = None
-        util.mkdir_and_rename(opt['path']['experiments_root'])  # rename old folder if exists
-        util.mkdirs((path for key, path in opt['path'].items() if not key == 'experiments_root'
-                     and 'pretrain_model' not in key and 'resume' not in key))
-
+    
     # config loggers. Before it, the log will not work
     util.setup_logger(None, opt['path']['log'], 'train', level=logging.INFO, screen=True)
     util.setup_logger('val', opt['path']['log'], 'val', level=logging.INFO)
     logger = logging.getLogger('base')
-
-    # resume training
+    
+    # train from scratch OR resume training
+    """
+    if opt['path']['resume_state']:  # resuming training
+        resume_state = torch.load(opt['path']['resume_state'])
+    """
     if opt['path']['resume_state']:
         if os.path.isdir(opt['path']['resume_state']):
-            opt['path']['resume_state'] = util.sorted_nicely(glob.glob(os.path.normpath(opt['path']['resume_state']) + '/*.state'))[-1]
-            logger.info('Set [resume_state] to ' + opt['path']['resume_state'])
-        resume_state = torch.load(opt['path']['resume_state'])
+            import glob
+            resume_state_path = util.sorted_nicely(glob.glob(os.path.normpath(opt['path']['resume_state']) + '/*.state'))[-1]
+            logger.info('Set [resume_state] to ' + resume_state_path)
+        else:
+            resume_state_path = opt['path']['resume_state']
+        resume_state = torch.load(resume_state_path)
+    else:  # training from scratch
+        resume_state = None
+        util.mkdir_and_rename(opt['path']['experiments_root'])  # rename old folder if exists
+        util.mkdirs((path for key, path in opt['path'].items() if not key == 'experiments_root'
+                     and 'pretrain_model' not in key and 'resume' not in key))
 
     if resume_state:
         logger.info('Resuming training from epoch: {}, iter: {}.'.format(
@@ -103,7 +107,7 @@ def main():
     # training
     logger.info('Start training from epoch: {:d}, iter: {:d}'.format(start_epoch, current_step))
     for epoch in range(start_epoch, total_epochs):
-        for n, train_data in enumerate(train_loader, 1):
+        for n, train_data in enumerate(train_loader,start=1):
             current_step += 1
             if current_step > total_iters:
                 break
@@ -120,7 +124,7 @@ def main():
                 message = '<epoch:{:3d}, iter:{:8,d}, lr:{:.3e}> '.format(
                     epoch, current_step, model.get_current_learning_rate())
                 for k, v in logs.items():
-                    message += '{:s}:{: .4e} '.format(k, v)
+                    message += '{:s}: {:.4e} '.format(k, v)
                     # tensorboard logger
                     if opt['use_tb_logger'] and 'debug' not in opt['name']:
                         tb_logger.add_scalar(k, v, current_step)
@@ -134,7 +138,9 @@ def main():
             
             # validation
             if current_step % opt['train']['val_freq'] == 0:
-                avg_psnr = 0.0
+                avg_psnr_c = 0.0
+                avg_psnr_s = 0.0
+                avg_psnr_p = 0.0
                 idx = 0
                 for val_data in val_loader:
                     idx += 1
@@ -146,7 +152,9 @@ def main():
                     model.test()
 
                     visuals = model.get_current_visuals()
-                    
+                    """
+                    sr_img = util.tensor2img(visuals['SR'])  # uint8
+                    """
                     img_c = util.tensor2img(visuals['img_c'])  # uint8
                     img_s = util.tensor2img(visuals['img_s'])  # uint8
                     img_p = util.tensor2img(visuals['img_p'])  # uint8
@@ -154,6 +162,12 @@ def main():
                     gt_img = util.tensor2img(visuals['HR'])  # uint8
 
                     # Save SR images for reference
+                    """
+                    save_img_path = os.path.join(img_dir, '{:s}_{:d}.png'.format(\
+                        img_name, current_step))
+                    util.save_img(sr_img, save_img_path)
+                    """
+                    
                     save_c_img_path = os.path.join(img_dir, '{:s}_{:d}_c.png'.format(img_name, current_step))
                     save_s_img_path = os.path.join(img_dir, '{:s}_{:d}_s.png'.format(img_name, current_step))
                     save_p_img_path = os.path.join(img_dir, '{:s}_{:d}_d.png'.format(img_name, current_step))
@@ -165,21 +179,39 @@ def main():
                     # calculate PSNR
                     crop_size = opt['scale']
                     gt_img = gt_img / 255.
-                    sr_img = img_c / 255. 
-                    cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                    #sr_img = sr_img / 255. #ESRGAN 
+                    #PPON
+                    #C
+                    sr_img_c = img_c / 255.
+                    cropped_sr_img_c = sr_img_c[crop_size:-crop_size, crop_size:-crop_size, :]
                     cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                    avg_psnr += util.calculate_psnr(cropped_sr_img * 255, cropped_gt_img * 255)
+                    avg_psnr_c += util.calculate_psnr(cropped_sr_img_c * 255, cropped_gt_img * 255)
+                    #S
+                    sr_img_s = img_s / 255.
+                    cropped_sr_img_s = sr_img_s[crop_size:-crop_size, crop_size:-crop_size, :]
+                    avg_psnr_s += util.calculate_psnr(cropped_sr_img_s * 255, cropped_gt_img * 255)
+                    #D
+                    sr_img_p = img_p / 255.
+                    cropped_sr_img_p = sr_img_p[crop_size:-crop_size, crop_size:-crop_size, :]
+                    avg_psnr_p += util.calculate_psnr(cropped_sr_img_p * 255, cropped_gt_img * 255)
+                    
 
-                avg_psnr = avg_psnr / idx
+                avg_psnr_c = avg_psnr_c / idx
+                avg_psnr_s = avg_psnr_s / idx
+                avg_psnr_p = avg_psnr_p / idx
 
                 # log
-                logger.info('# Validation # PSNR: {:.5g}'.format(avg_psnr))
+                logger.info('# Validation # PSNR_c: {:.4e}'.format(avg_psnr_c))
+                logger.info('# Validation # PSNR_s: {:.4e}'.format(avg_psnr_s))
+                logger.info('# Validation # PSNR_p: {:.4e}'.format(avg_psnr_p))
                 logger_val = logging.getLogger('val')  # validation logger
-                logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.5g}'.format(
-                    epoch, current_step, avg_psnr))
+                logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr_c: {:.4e}, psnr_s: {:.4e}, psnr_p: {:.4e}'.format(
+                    epoch, current_step, avg_psnr_c, avg_psnr_s, avg_psnr_p))
                 # tensorboard logger
                 if opt['use_tb_logger'] and 'debug' not in opt['name']:
-                    tb_logger.add_scalar('psnr', avg_psnr, current_step)
+                    tb_logger.add_scalar('psnr_c', avg_psnr_c, current_step)
+                    tb_logger.add_scalar('psnr_s', avg_psnr_s, current_step)
+                    tb_logger.add_scalar('psnr_p', avg_psnr_p, current_step)
 
 
     logger.info('Saving the final model.')
