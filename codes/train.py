3e0a1dada04bd15719418aff8a16e534cbe86da0
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2020-01-27 17:57:28 -0700

    Circumvent pytorch warning
    We pass current_step to scheduler.step()
    The warning about scheduler/optimizer order doesn't apply here

--- codes/train.py
+++ codes/train.py
@@ -16,25 +16,12 @@ from data import create_dataloader, create_dataset
 from models import create_model
 from models.modules.LPIPS import compute_dists as lpips
 
-def get_pytorch_ver():
-    #print(torch.__version__)
-    pytorch_ver = torch.__version__
-    if pytorch_ver == "0.4.0":
-        return "pre"
-    elif pytorch_ver == "0.4.1":
-        return "pre"
-    elif pytorch_ver == "1.0.0":
-        return "pre"
-    else: #"1.1.0", "1.1.1", "1.2.0", "1.2.1" and beyond
-        return "post"
-        
 def main():
     # options
     parser = argparse.ArgumentParser()
     parser.add_argument('-opt', type=str, required=True, help='Path to option YAML file.')
     opt = option.parse(parser.parse_args().opt, is_train=True)
     opt = option.dict_to_nonedict(opt)  # Convert to NoneDict, which return None for missing key.
-    pytorch_ver = get_pytorch_ver()
     
     # train from scratch OR resume training
     if opt['path']['resume_state']:
@@ -101,6 +88,12 @@ def main():
     # create model
     model = create_model(opt)
 
+    # circumvent pytorch warning
+    # we pass in the iteration count to scheduler.step(), so the warning doesn't apply
+    for scheduler in model.schedulers:
+        if hasattr(scheduler, '_step_count'):
+            scheduler._step_count = 0
+
     # resume training
     if resume_state:
         start_epoch = resume_state['epoch']
@@ -119,22 +112,12 @@ def main():
             current_step += 1
             if current_step > total_iters:
                 break
-            
-            if pytorch_ver=="pre": #Order for PyTorch ver < 1.1.0
-                # update learning rate
-                model.update_learning_rate(current_step-1)
-                # training
-                model.feed_data(train_data)
-                model.optimize_parameters(current_step)
-            elif pytorch_ver=="post": #Order for PyTorch ver > 1.1.0
-                # training
-                model.feed_data(train_data)
-                model.optimize_parameters(current_step)
-                # update learning rate
-                model.update_learning_rate(current_step-1)
-            else:
-                print('Error identifying PyTorch version. ', torch.__version__)
-                break
+            # update learning rate
+            model.update_learning_rate(current_step-1)
+
+            # training
+            model.feed_data(train_data)
+            model.optimize_parameters(current_step)
 
             # log
             if current_step % opt['logger']['print_freq'] == 0:
