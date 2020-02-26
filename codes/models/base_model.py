a02f82530b56b6f5a5a5cbf1e38ec62f91fdee20
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2020-02-26 13:33:41 -0700

    Fix pytorch 1.4.0 compatibility and bug

--- codes/models/base_model.py
+++ codes/models/base_model.py
@@ -47,8 +47,12 @@ class BaseModel():
             else:
                 scheduler.step()
 
-    def get_current_learning_rate(self):
-        return self.schedulers[0].get_lr()[0]
+    if torch.__version__ >= '1.4.0':
+        def get_current_learning_rate(self):
+            return self.schedulers[0].get_last_lr()[0]
+    else:
+        def get_current_learning_rate(self):
+            return self.schedulers[0].get_lr()[0]
 
     def get_network_description(self, network):
         '''Get the string and total parameters of the network'''
