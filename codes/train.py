677a780866c8d21d430ea6daf5d7c04f076908f4
PHOENiX <rlaphoenix@pm.me>
2020-12-30 09:48:53 +0000

    codes.train: Remove unnecessary comments and don't store the Trainer instance

--- codes/train.py
+++ codes/train.py
@@ -4,12 +4,10 @@ from codes.scripts.classes.trainer import Trainer
 
 
 def main():
-    # arguments
     args = argparse.ArgumentParser()
     args.add_argument('-i', type=str, required=True, help='Path to option JSON file.')
     args = args.parse_args()
-
-    trainer = Trainer(args.i)
+    Trainer(args.i)
 
 
 if __name__ == '__main__':
