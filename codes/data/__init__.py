f16fb688146789cac9f38d8a0fa7d36ecbd8c521
PHOENiX <rlaphoenix@pm.me>
2020-12-30 04:08:41 +0000

    codes.data.__init__: Clean up syntax, code, doc-strings, type hinting

--- codes/data/__init__.py
+++ codes/data/__init__.py
@@ -1,47 +1,59 @@
-'''create dataset and dataloader'''
+"""Create dataset and dataloader"""
 import logging
+
 import torch.utils.data
 
-def create_dataloader(dataset, dataset_opt):
-    '''create dataloader '''
-    phase = dataset_opt['phase']
-    if phase == 'train':
-        return torch.utils.data.DataLoader(
-            dataset,
-            batch_size=dataset_opt['batch_size'],
-            shuffle=dataset_opt['use_shuffle'],
-            num_workers=dataset_opt['n_workers'],
-            drop_last=True,
-            pin_memory=True)
-    else:
-        return torch.utils.data.DataLoader(
-            dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+
+def create_dataloader(dataset: torch.utils.data.Dataset, dataset_opt: dict) -> torch.utils.data.DataLoader:
+    """
+    Create Dataloader
+    :param dataset: Dataset to use
+    :param dataset_opt: Dataset configuration from opt file
+    """
+    batch_size = 1
+    shuffle = False
+    num_workers = 0
+    drop_last = False
+    if dataset_opt['phase'] == 'train':
+        batch_size = dataset_opt['batch_size']
+        shuffle = dataset_opt['use_shuffle']
+        num_workers = dataset_opt['n_workers']
+        drop_last = True
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        drop_last=drop_last,
+        pin_memory=True
+    )
 
 
-def create_dataset(dataset_opt):
-    '''create dataset'''
-    mode = dataset_opt['mode']
-    if mode == 'LR':
-        from data.LR_dataset import LRDataset as D
-    elif mode == 'LRHR':
-        from data.LRHR_dataset import LRHRDataset as D
-    elif mode == 'LRHROTF':
-        from data.LRHROTF_dataset import LRHRDataset as D
-    elif mode == 'LRHRC':
-        from data.LRHRC_dataset import LRHRDataset as D
-    elif mode == 'LRHRseg_bg':
-        from data.LRHR_seg_bg_dataset import LRHRSeg_BG_Dataset as D
-    elif mode == 'VLRHR':
-        from data.Vid_dataset import VidTrainsetLoader as D
-    elif mode == 'VLR':
-        from data.Vid_dataset import VidTestsetLoader as D
-    elif mode == 'LRHRPBR':
-        from data.LRHRPBR_dataset import LRHRDataset as D
+def create_dataset(dataset_opt: dict) -> torch.utils.data.Dataset:
+    """
+    Create Dataset
+    :param dataset_opt: Dataset configuration from opt file
+    """
+    m = dataset_opt['mode']
+    if m == 'LR':
+        from codes.data.LR_dataset import LRDataset as Dataset
+    elif m == 'LRHR':
+        from codes.data.LRHR_dataset import LRHRDataset as Dataset
+    elif m == 'LRHROTF':
+        from codes.data.LRHROTF_dataset import LRHRDataset as Dataset
+    elif m == 'LRHRC':
+        from codes.data.LRHRC_dataset import LRHRDataset as Dataset
+    elif m == 'LRHRseg_bg':
+        from codes.data.LRHR_seg_bg_dataset import LRHRSeg_BG_Dataset as Dataset
+    elif m == 'VLRHR':
+        from codes.data.Vid_dataset import VidTrainsetLoader as Dataset
+    elif m == 'VLR':
+        from codes.data.Vid_dataset import VidTestsetLoader as Dataset
+    elif m == 'LRHRPBR':
+        from codes.data.LRHRPBR_dataset import LRHRDataset as Dataset
     else:
-        raise NotImplementedError('Dataset [{:s}] is not recognized.'.format(mode))
-    dataset = D(dataset_opt)
+        raise NotImplementedError('Dataset [%s] is not recognized.' % m)
+    dataset = Dataset(dataset_opt)
     logger = logging.getLogger('base')
-    logger.info('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__,
-                                                           dataset_opt['name']))
+    logger.info('Dataset [%s - %s] is created.' % (dataset.__class__.__name__, dataset_opt['name']))
     return dataset
-
