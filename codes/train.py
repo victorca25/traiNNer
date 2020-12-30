d99fc48f23f72601f6cd6f3335964ea7a7cc8afb
PHOENiX <rlaphoenix@pm.me>
2020-12-30 09:55:46 +0000

    codes.train: Remove the term JSON from argument help, since YAML is also used
    It's best not to refer to either directly, too.

--- codes/train.py
+++ codes/train.py
@@ -5,7 +5,7 @@ from codes.runner.trainer import Trainer
 
 def main():
     args = argparse.ArgumentParser()
-    args.add_argument('-i', type=str, required=True, help='Path to option JSON file.')
+    args.add_argument('-i', type=str, required=True, help='Path to option file.')
     args = args.parse_args()
     Trainer(args.i)
 
