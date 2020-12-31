9c1823523edbfd707cc78f3ea5362454e96445c1
PHOENiX <rlaphoenix@pm.me>
2020-12-31 00:38:02 +0000

    metrics.calculate_PSNR_SSIM: Remove the duplicate calculate functions

--- metrics/calculate_PSNR_SSIM.py
+++ metrics/calculate_PSNR_SSIM.py
@@ -6,11 +6,12 @@ Terms:
     Gen: Generated/Restored/Recovered images
 """
 import glob
-import math
 import os
 
 import cv2
 import numpy as np
+
+from codes.utils.metrics import calculate_psnr, calculate_ssim
 
 
 def main():
@@ -55,62 +56,6 @@ def main():
     print('Average: PSNR: {:.6f} dB, SSIM: {:.6f}'.format(
         sum(psnr_all) / len(psnr_all), sum(ssim_all) / len(ssim_all))
     )
-
-
-def calculate_psnr(img_1: np.ndarray, img_2: np.ndarray) -> float:
-    """
-    Calculate PSNR
-    :param img_1: Image 1 [0, 255]
-    :param img_2: Image 2 [0, 255]
-    """
-    img_1 = img_1.astype(np.float64)
-    img_2 = img_2.astype(np.float64)
-    mse = np.mean((img_1 - img_2) ** 2)
-    if mse == 0:
-        return float('inf')
-    return 20 * math.log10(255.0 / math.sqrt(mse))
-
-
-def calculate_ssim(img1: np.ndarray, img2: np.ndarray) -> float:
-    """
-    Calculate SSIM
-    The same outputs as MATLAB's
-    :param img1: Image 1 [0, 255]
-    :param img2: Image 2 [0, 255]
-    """
-
-    def ssim(img_1: np.ndarray, img_2: np.ndarray) -> float:
-        c1 = (0.01 * 255) ** 2
-        c2 = (0.03 * 255) ** 2
-
-        img_1 = img_1.astype(np.float64)
-        img_2 = img_2.astype(np.float64)
-        kernel = cv2.getGaussianKernel(11, 1.5)
-        window = np.outer(kernel, kernel.transpose())
-
-        mu1 = cv2.filter2D(img_1, -1, window)[5:-5, 5:-5]  # valid
-        mu2 = cv2.filter2D(img_2, -1, window)[5:-5, 5:-5]
-        mu1_sq = mu1 ** 2
-        mu2_sq = mu2 ** 2
-        mu1_mu2 = mu1 * mu2
-        sigma1_sq = cv2.filter2D(img_1 ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
-        sigma2_sq = cv2.filter2D(img_2 ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
-        sigma12 = cv2.filter2D(img_1 * img_2, -1, window)[5:-5, 5:-5] - mu1_mu2
-
-        ssim_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2))
-        return ssim_map.mean()
-
-    if not img1.shape == img2.shape:
-        raise ValueError('Input images must have the same dimensions.')
-    if img1.ndim == 2:
-        return ssim(img1, img2)
-    elif img1.ndim == 3:
-        if img1.shape[2] == 3:
-            return np.array([ssim(img1, img2) for _ in range(3)]).mean()
-        elif img1.shape[2] == 1:
-            return ssim(np.squeeze(img1), np.squeeze(img2))
-    else:
-        raise ValueError('Wrong input image dimensions.')
 
 
 def bgr2ycbcr(img: np.ndarray, only_y: bool = True) -> np.ndarray:
