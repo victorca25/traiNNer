bf059b15795cf1a6ddd8e06523541eb5ccccb915
PHOENiX <rlaphoenix@pm.me>
2021-01-01 18:50:42 +0000

    metrics: Clean up MetricsDict, and PSNR SSIM functions

--- codes/utils/metrics.py
+++ codes/utils/metrics.py
@@ -16,46 +16,31 @@ class MetricsDict:
     def __init__(self, metrics='psnr', lpips_model=None):
         metrics = metrics.lower()
         self.count = 0
-        self.psnr = None
-        self.ssim = None
-        self.lpips = None
+        self.metrics_list = [x.lower() for x in metrics.split(',')]
+        self.psnr = 'psnr' in self.metrics_list
+        self.psnr_sum = 0
+        self.ssim = 'ssim' in self.metrics_list
+        self.ssim_sum = 0
+        self.lpips = 'lpips' in self.metrics_list
+        self.lpips_sum = 0
+        self.lpips_model = lpips_model
 
-        self.metrics_list = []
-        for metric in metrics.split(','):  # default='psnr' +
-            if metric == 'psnr':
-                self.psnr = True
-                self.metrics_list.append({'name': 'psnr'})
-                self.psnr_sum = 0
-            if metric == 'ssim':
-                self.ssim = True
-                self.metrics_list.append({'name': 'ssim'})
-                self.ssim_sum = 0
+        if self.lpips and not self.lpips_model:
             # LPIPS only works for RGB images
-            if metric == 'lpips':
-                self.lpips = True
-                if not lpips_model:
-                    self.lpips_model = PerceptualLoss(
-                        model='net-lin',
-                        use_gpu=False,
-                        net='squeeze',
-                        spatial=False
-                    )
-                else:
-                    self.lpips_model = lpips_model
-                self.metrics_list.append({'name': 'lpips'})
-                self.lpips_sum = 0
+            self.lpips_model = PerceptualLoss(
+                model='net-lin',
+                use_gpu=False,
+                net='squeeze',
+                spatial=False
+            )
 
     def calculate_metrics(self, img1, img2, crop_size=4, only_y=False):
-        tensor_images = True if (isinstance(img1, torch.Tensor) and isinstance(img2, torch.Tensor)) else False
-
-        # if images are np arrays, the convention is that they are in the [0, 255] range
-        # tensor are in the [0, 1] range
-
         # TODO: should images be converted from tensor here?
-
-        if tensor_images:
+        if isinstance(img1, torch.Tensor) and isinstance(img2, torch.Tensor):
+            # expecting tensor in [0, 1] range
             pass  # TODO
         else:
+            # expecting np array in [0, 255] range
             if only_y:
                 img1 = bgr2ycbcr(img1, only_y=True)
                 img2 = bgr2ycbcr(img2, only_y=True)
@@ -64,32 +49,20 @@ class MetricsDict:
             img1 = img1[crop_size:-crop_size, crop_size:-crop_size, ...]
             img2 = img2[crop_size:-crop_size, crop_size:-crop_size, ...]
 
-            for _, m in enumerate(self.metrics_list):
-                if m['name'] == 'psnr':
-                    self.psnr_total(calculate_psnr(img1, img2, False))
-                elif m['name'] == 'ssim':
-                    self.ssim_total(calculate_ssim(img1, img2, False))
-                elif m['name'] == 'lpips' and not only_y:  # single channel images not supported by LPIPS
-                    self.lpips_total(calculate_lpips([img1], [img2], model=self.lpips_model).item())
+            for m in self.metrics_list:
+                if m == 'psnr':
+                    self.psnr_sum += calculate_psnr(img1, img2, False)
+                elif m == 'ssim':
+                    self.ssim_sum += calculate_ssim(img1, img2, False)
+                elif m == 'lpips' and not only_y:  # single channel images not supported by LPIPS
+                    self.lpips_sum += calculate_lpips([img1], [img2], model=self.lpips_model).item()
         self.count += 1
 
     def reset(self):
         self.count = 0
-        if self.psnr:
-            self.psnr_sum = 0
-        if self.ssim:
-            self.ssim_sum = 0
-        if self.lpips:
-            self.lpips_sum = 0
-
-    def psnr_total(self, value):
-        self.psnr_sum += value
-
-    def ssim_total(self, value):
-        self.ssim_sum += value
-
-    def lpips_total(self, value):
-        self.lpips_sum += value
+        self.psnr_sum = 0
+        self.ssim_sum = 0
+        self.lpips_sum = 0
 
     def get_averages(self):
         averages_list = []
@@ -103,21 +76,25 @@ class MetricsDict:
         return averages_list
 
 
-# Matlab removes the border before calculating PSNR and SSIM
-def calculate_psnr(img1, img2, shave=4):  # numpy
+def calculate_psnr(a: np.ndarray, b: np.ndarray, shave: int = 4) -> float:
+    """
+    Calculate PSNR - the same output as MATLAB.
+    :param a: Numpy Array in range [0, 255]
+    :param b: Numpy Array in range [0, 255]
+    :param shave: Shave/Crop some of the edges off the input image.
+    """
+    # MATLAB version removes the border before calculating PSNR
     if shave:
-        img1 = img1[shave:-shave, shave:-shave, ...]
-        img2 = img2[shave:-shave, shave:-shave, ...]
-    # img1 and img2 have range [0, 255]
-    img1 = img1.astype(np.float64)
-    img2 = img2.astype(np.float64)
-    mse = np.mean((img1 - img2) ** 2)
+        a = a[shave:-shave, shave:-shave, ...]
+        b = b[shave:-shave, shave:-shave, ...]
+    a = a.astype(np.float64)
+    b = b.astype(np.float64)
+    mse = np.mean((a - b) ** 2)
     if mse == 0:
         return float('inf')
     return 20 * math.log10(255.0 / math.sqrt(mse))
 
 
-# Note: the output of the single scalar value coincides with the numpy version, but the per batch image results don't appear to coincide
 def calculate_psnr_torch(img1, img2, clip=False, max_val=1., only_y=False, single=False, shave=4):
     """Returns the Peak Signal-to-Noise Ratio between img1 and img2.
     This is intended to be used on signals (or images). Produces a PSNR value for
@@ -135,6 +112,8 @@ def calculate_psnr_torch(img1, img2, clip=False, max_val=1., only_y=False, singl
       The scalar PSNR between img1 and img2. The returned tensor
       has shape [batch_size, 1].
     """
+    # Note: the output of the single scalar value coincides with the numpy version, but the per batch image results
+    # don't appear to coincide
     # clip assumes image in range [0,1]
     if img1.shape != img2.shape:
         raise TypeError("Expected tensors of equal shapes, but got %s and %s" % (img1.shape, img2.shape))
@@ -167,50 +146,51 @@ def calculate_psnr_torch(img1, img2, clip=False, max_val=1., only_y=False, singl
     return 10 * torch.log10(max_val_tensor ** 2 / mse)
 
 
-def ssim(img1, img2):
-    C1 = (0.01 * 255) ** 2
-    C2 = (0.03 * 255) ** 2
+def ssim(a, b):
+    c1 = (0.01 * 255) ** 2
+    c2 = (0.03 * 255) ** 2
 
-    img1 = img1.astype(np.float64)
-    img2 = img2.astype(np.float64)
+    a = a.astype(np.float64)
+    b = b.astype(np.float64)
 
     kernel = cv2.getGaussianKernel(11, 1.5)
     window = np.outer(kernel, kernel.transpose())
 
-    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
-    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1 = cv2.filter2D(a, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(b, -1, window)[5:-5, 5:-5]
     mu1_sq = mu1 ** 2
     mu2_sq = mu2 ** 2
     mu1_mu2 = mu1 * mu2
-    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
-    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
-    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+    sigma1_sq = cv2.filter2D(a ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(b ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(a * b, -1, window)[5:-5, 5:-5] - mu1_mu2
 
-    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
-                                                            (sigma1_sq + sigma2_sq + C2))
+    ssim_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2))
     return ssim_map
 
 
-def calculate_ssim(img1, img2, shave=4):
-    '''calculate SSIM
-    the same outputs as MATLAB's
-    img1, img2: [0, 255]
-    '''
-    if not img1.shape == img2.shape:
+def calculate_ssim(a: np.ndarray, b: np.ndarray, shave: int = 4) -> float:
+    """
+    Calculate SSIM - the same output as MATLAB.
+    :param a: Numpy Array in range [0, 255]
+    :param b: Numpy Array in range [0, 255]
+    :param shave: Shave/Crop some of the edges off the input image.
+    """
+    # MATLAB version removes the border before calculating PSNR
+    if a.shape != b.shape:
         raise ValueError('Input images must have the same dimensions.')
 
-    if shave and img1.ndim == 3:
-        img1 = img1[shave:-shave, shave:-shave, ...]
-        img2 = img2[shave:-shave, shave:-shave, ...]
+    if shave and a.ndim == 3:
+        a = a[shave:-shave, shave:-shave, ...]
+        b = b[shave:-shave, shave:-shave, ...]
 
-    if img1.ndim == 2:
-        return ssim(img1, img2).mean()
-    elif img1.ndim == 3:
-        if img1.shape[2] == 3:
-            ssims = ssim(img1, img2)
-            return ssims.mean()
-        elif img1.shape[2] == 1:
-            return ssim(np.squeeze(img1), np.squeeze(img2))
+    if a.ndim == 2:
+        return ssim(a, b).mean()
+    elif a.ndim == 3:
+        if a.shape[2] == 3:
+            return ssim(a, b).mean()
+        elif a.shape[2] == 1:
+            return ssim(np.squeeze(a), np.squeeze(b))
     else:
         raise ValueError('Wrong input image dimensions.')
 
