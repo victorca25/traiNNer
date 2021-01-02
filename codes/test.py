7139cdfcc21d33cdd947a484fa1d8c5eacd76495
PHOENiX <rlaphoenix@pm.me>
2021-01-02 21:08:36 +0000

    codes.test: Pass the wanted visuals and metric visual keys

--- codes/test.py
+++ codes/test.py
@@ -7,7 +7,13 @@ def main():
     args = argparse.ArgumentParser()
     args.add_argument('-i', type=str, required=True, help='Path to option file.')
     args = args.parse_args()
-    Tester(args.i, 'LR_path')  # might not use this key, depends what Dataset is used.
+    Tester(
+        args.i,
+        # TODO: Might not use these keys, depends what Dataset is used.
+        #       These keys are being used, as that's what was used in the old test.py
+        path_key='LR_path',
+        wanted_visuals=['SR'],
+        metric_visuals=('SR', 'HR'))
 
 
 if __name__ == '__main__':
