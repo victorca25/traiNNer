e6e788b0f8cd374d2fb59f59e198910f482a00ca
PHOENiX <rlaphoenix@pm.me>
2021-01-02 21:09:05 +0000

    codes.test_dvd: Pass the wanted visuals and metric visual keys

--- codes/test_dvd.py
+++ codes/test_dvd.py
@@ -7,7 +7,12 @@ def main():
     args = argparse.ArgumentParser()
     args.add_argument('-i', type=str, required=True, help='Path to option file.')
     args = args.parse_args()
-    Tester(args.i, 'in_path')
+    Tester(
+        args.i,
+        path_key='in_path',
+        wanted_visuals=['top_fake', 'bottom_fake'],
+        metric_visuals=('top_fake', 'top_real')
+    )
 
 
 if __name__ == '__main__':
